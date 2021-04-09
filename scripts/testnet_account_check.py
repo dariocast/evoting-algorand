4a5444ce54c3ac6b2824704f3833d1525f30a388
Dario Castellano <dariocastellano18@gmail.com>
2021-04-09 13:26:14 +0200

    feat: scripts

--- scripts/testnet_account_check.py
+++ scripts/testnet_account_check.py
@@ -1,4 +1,4 @@
-from scripts.testnet_client import getAlgodClient
+from testnet_client import getAlgodClient
 from algosdk import account, mnemonic
 import json
 
@@ -9,14 +9,20 @@ def getPassphrases():
     file = os.path.join(THIS_FOLDER, 'config.json')
     with open(file, "r") as config_file:
         data = json.load(config_file)
-        for passphrase in data:
-            passphrases.append(data['passphrase'])
+        for account in data['accounts']:
+            passphrases.append(account['passphrase'])
         return passphrases
 
-passphrases = getPassphrases()
-private_key = mnemonic.to_private_key(passphrases[0])
-my_address = mnemonic.to_public_key(passphrases[0])
-print("My address: {}".format(my_address))
 
-account_info = getAlgodClient.account_info(my_address)
-print("Account balance: {} microAlgos".format(account_info.get('amount')))
+passphrases = getPassphrases()
+private_key1 = mnemonic.to_private_key(passphrases[0])
+private_key2 = mnemonic.to_private_key(passphrases[1])
+my_address1 = mnemonic.to_public_key(passphrases[0])
+my_address2 = mnemonic.to_public_key(passphrases[1])
+print("First address: {}".format(my_address1))
+print("Second address: {}".format(my_address2))
+
+account_info = getAlgodClient().account_info(my_address1)
+account_info2 = getAlgodClient().account_info(my_address2)
+print("First account balance: {} microAlgos".format(account_info.get('amount')))
+print("Second account balance: {} microAlgos".format(account_info2.get('amount')))
--- scripts/testnet_client.py
+++ scripts/testnet_client.py
@@ -1,13 +1,14 @@
 from algosdk.v2client import algod
 import json
 
-def getAlgodClient():
+
+def getAlgodClient() -> algod.AlgodClient:
     import os
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     file = os.path.join(THIS_FOLDER, 'config.json')
     with open(file, "r") as config_file:
         data = json.load(config_file)
-        algod_address = 'http://'+ data['macmini']['host'] + ':' + str(data['macmini']['port'])
+        algod_address = 'http://' + data['macmini']['host'] + ':' + str(data['macmini']['port'])
         algod_token = data['macmini']['token']
         return algod.AlgodClient(algod_address=algod_address, algod_token=algod_token)
 
@@ -15,4 +16,3 @@ def getAlgodClient():
 client = getAlgodClient()
 status = client.status()
 print(json.dumps(status, indent=4))
-
