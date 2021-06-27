6dc16aac94c2fb808295475063cb2a778154f33f
Dario Castellano <dario.castellano@etuitus.it>
2021-06-27 17:05:59 +0200

    feat: add qr visualization of address

--- flutter_app/lib/modules/home/screens/home_page.dart
+++ flutter_app/lib/modules/home/screens/home_page.dart
@@ -5,7 +5,9 @@ import 'package:algorand_evoting/modules/modules.dart';
 import 'package:algorand_evoting/config/themes/themes.dart';
 import 'package:algorand_evoting/modules/voting_creation/voting_creation.dart';
 import 'package:algorand_evoting/modules/voting_detail/voting_detail.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomePage extends StatelessWidget {
   static Route route() {
@@ -31,6 +33,67 @@ class HomePage extends StatelessWidget {
                   HomeStarted(),
                 ),
           ),
+          IconButton(
+              icon: Icon(Icons.qr_code),
+              onPressed: () {
+                showModalBottomSheet(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    context: context,
+                    builder: (context) {
+                      AccountLoaded state =
+                          context.read<AccountBloc>().state as AccountLoaded;
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'This is your address, use it to receive voting tokens',
+                                style: TextStyle(
+                                  fontSize: fontSizeMedium,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: InkWell(
+                                onTap: () {
+                                  Clipboard.setData(ClipboardData(
+                                      text: state.account!.publicAddress));
+                                  Navigator.pop(context);
+                                  ScaffoldMessenger.of(context)
+                                    ..hideCurrentSnackBar()
+                                    ..showSnackBar(SnackBar(
+                                      content: Text('Copied to clipboard'),
+                                      duration: Duration(seconds: 1),
+                                    ));
+                                },
+                                child: Text(
+                                  state.account!.publicAddress,
+                                  style: TextStyle(
+                                    fontSize: fontSizeMedium,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 30.0,
+                                  right: 30.0,
+                                  top: 8.0,
+                                  bottom: 8.0),
+                              child:
+                                  QrImage(data: state.account!.publicAddress),
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              }),
           IconButton(
             icon: Icon(Icons.logout),
             color: Colors.red,
