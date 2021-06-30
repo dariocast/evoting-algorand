f62d4558478146b989e997c04a49d3a2370adca4
Dario Castellano <dario.castellano@etuitus.it>
2021-06-30 14:04:23 +0200

    style (voting_details): refactor ui

--- flutter_app/lib/core/models/simple_assets.dart
+++ flutter_app/lib/core/models/simple_assets.dart
@@ -4,15 +4,17 @@ class SimpleAsset extends Equatable {
   final String name;
   final int id;
   final int balance;
+  final String unitName;
   final bool isCreator;
 
   const SimpleAsset({
     required this.name,
     required this.id,
     required this.balance,
+    required this.unitName,
     required this.isCreator,
   });
 
   @override
-  List<Object?> get props => [name, id, balance, isCreator];
+  List<Object?> get props => [name, id, balance, isCreator, unitName];
 }
